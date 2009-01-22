09f7e7fd0a50d90dd342ea0e01b2c6f4ed0e5e40
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2009-01-22 13:31:45 -0800

    Add shared.py for shared agent class functions.
    Shared will contain the function for generating unique IDs, initializing
    booleans, etc.

---
+++ agents/shared.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""
+Part of Chitwan Valley agent-based model.
+
+Contains classes/functions shared among the different agent classes.
+
+Alex Zvoleff, azvoleff@mail.sdsu.edu
+"""
+
+import numpy as np
+
+class IDGenerator(object):
+    "A generator class for consecutive unique ID numbers."
+    def __init__(self):
+        self._PID = -1
+
+    def next(self):
+        self._PID += 1
+        return self._PID
+
+def Boolean(trueProb = .5):
+    if np.random.rand() < trueProb:
+        return True
+    else:
+        return False
