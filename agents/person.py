3c1f534bd7f8d220b1b47521c1fe26ea5a7577c1
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2009-01-27 14:15:35 -0800

    Continue coding person class.

--- agents/person.py
+++ agents/person.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """
 Part of Chitwan Valley agent-based model.
 
@@ -7,22 +6,46 @@ Class for person agents.
 Alex Zvoleff, azvoleff@mail.sdsu.edu
 """
 
-import shared
+PIDGen = IDGenerator()
 
-PIDGen = shared.IDGenerator()
+class Population(object):
+    "Represents a set of persons sharing certain demographic characteristics."
+    def __init__(self):
+        self._persons = {}
+
+    def getCouple(self):
+        # couples will start out as a list of one spouse from each couple, then 
+        # be extended to become a list of couples
+        couples = []
+        for person in self._persons():
+            if person.is_married() and person.getSpousePID:
+                # store the first spouse
+                couples.append(person)
+
+        # Now store the second spouse
+        #for couple in self.couples():
+        #    couple.extend(self._persons[couple.
 
 class Person(object):
     "Represents a single person agent"
-    def __init__(self, birthday):
+    def __init__(self, birthday, age=None):
+        # birthday is the timestep of the birth of the agent. It is used to 
+        # calculate the age of the agent. birthdays can be negative for those 
+        # agents used to initialize the model, to represent agents who existed 
+        # prior to time 0
         self._birthDate = birthday
 
         self._PID = PIDGen.next()
-        self._age = None #TODO
         self._sex = None #TODO
-        self._spousePID = None #TODO
 
     def get_PID(self):
         return self._PID
+
+    def get_spouse_PID(self):
+        try:
+            return self._spousePID
+        except AttributeError:
+            raise AttributeError("spouse of agent %i is not defined"%self.get_PID())
 
     def marry(self, spouse):
         "Marries this agent to another Person instance."
