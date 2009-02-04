943f8137fb7b2f9ba6f01398a98850a0d19604cc
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2009-02-03 23:07:07 -0800

    Add skeleton to runmodel.py.

--- runmodel.py
+++ runmodel.py
@@ -10,8 +10,37 @@ NOTE: Borrows code from matplotlib, particularly for rcsetup functions.
 Alex Zvoleff, azvoleff@mail.sdsu.edu
 """
 
+import os
+import sys
+import getopt
+import time
+import pickle
+
 from chitwanABM import rcParams
+
+modelRunStartTime = time.time()    
 
 if rcParams['model.use_psyco'] == True:
     import psyco
     psyco.full()
+
+def main():
+    # Initialize
+    
+    # Run the modellooop
+    
+    # Save the results
+
+    # After running model, save rcParams to a file, along with the SHA-1 of the 
+    # code version used to run it, and the start and finish times of the model 
+    # run. Save this file in the same folder as the model output.
+
+def elapsedTime(startTime):
+    elapsed = int(time.time() - startTime)
+    hours = elapsed / 3600
+    minutes = (elapsed - hours * 3600) / 60
+    seconds = elapsed - hours * 3600 - minutes * 60
+    return "%ih %im %is" %(hours, minutes, seconds)
+
+if __name__ == "__main__":
+    sys.exit(main())
