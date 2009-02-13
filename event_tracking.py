616e671231e51e733c6512e66be4443048bbeff2
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2009-02-13 15:26:34 -0800

    Add event tracking script.
    event_tracking.py will allow easier tracking of events (births, marriages,
    deaths, etc) for later analysis and plotting.

---
+++ event_tracking.py
@@ -0,0 +1,18 @@
+"""
+Part of Chitwan Valley agent-based model.
+
+Classes for life events such as marriage, birth, migration, etc. Allows easier 
+tracking of these events when plotting results.
+
+Alex Zvoleff, azvoleff@mail.sdsu.edu
+"""
+class Birth():
+    # TODO: Store parents PIDs, date, child PID
+class Death():
+    # TODO: Store person's PID, date, "cause"
+class Marriage():
+    # TODO: Store both spouse's PIDs, date
+class Migration():
+    # TODO: Store person's PID, date left, "reason"
+class MoveOut():
+    # TODO: Store person's PID, date left, "reason"
