71987d70cce09eebb930c356eab416a84a513aff
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2009-02-13 15:20:25 -0800

    Add DataStore class.
    DataStore class will store the results of each iteration of the model run for
    later debugging and testing.

--- modelloop.py
+++ modelloop.py
@@ -20,17 +20,34 @@ endtime = rcParams['model.endtime']
 timestep= rcParams['model.timestep']
 timesteps = np.arange(timezero, endtime, timestep)
 
+class DataStore:
+    """A class to store all agents and their attributes for each timestep of 
+    the model. Useful for later computing statistics or for debugging the 
+    model"""
+    def __init__():
+        self._landuse = []
+        self._region = []
+
+    def add_data(landuse, region):
+        self._landuse.append(landuse)
+        self._region.append(region)
+
 def mainloop(pop, land):
     """This function contains the main model loop. Passed to it is an instance 
     of the modelRun class, which contains parameters defining the size of each 
     timestep, the person, household, and neighborhood agents to be used in the 
     model, and the landuse parameters."""
 
+    # savedData will store the results of each timestep.
+    savedData = DataStore()
+
     for t in timeSteps()
-        pop.births()
-        pop.deaths()
-        pop.marriages()
-        pop.increment_age()
+        people.births()
+        people.deaths()
+        people.marriages()
+        people.increment_age()
 
         # Calculate and update land use
-        land.update(pop, time)
+        land.update(people, time)
+
+        savedData.addData(people, land)
