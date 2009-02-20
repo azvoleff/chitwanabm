9fc4f082e2d071407212d583040a0d1966a0b06b
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2009-02-19 16:06:34 -0800

    Add a landuse attribute to Region agents.

--- eventtracking.py
+++ eventtracking.py
@@ -6,13 +6,17 @@ tracking of these events when plotting results.
 
 Alex Zvoleff, azvoleff@mail.sdsu.edu
 """
-class Birth():
-    # TODO: Store parents PIDs, date, child PID
-class Death():
-    # TODO: Store person's PID, date, "cause"
-class Marriage():
-    # TODO: Store both spouse's PIDs, date
-class Migration():
-    # TODO: Store person's PID, date left, "reason"
-class MoveOut():
-    # TODO: Store person's PID, date left, "reason"
+class Birth(object):
+    # TODO: Store parents, date, child
+
+class Death(object):
+    # TODO: Store person, date
+
+class Marriage(object):
+    # TODO: Store both spouses, date
+
+class Migration(object):
+    # TODO: Store persons, date, "reason"
+
+class MoveOut(object):
+    # TODO: Store person, date, "reason"
--- landuse.py
+++ landuse.py
@@ -12,21 +12,21 @@ class LandUse():
     def __init__(self, coeff):
         'Initializes a land use type object.'
         self._time = []
-        self._proportion = []
+        self._proportions = []
         # 'coeffs' is a vector of OLS regression coefficients
         self._coeffs = coeff
 
     def add_value(self, time, proportion):
         'Adds a new landuse value in manually (without OLS)'
         self._time.append(time)
-        self._proportion(proportion)
+        self._proportions(proportion)
 
     def get_value(self, time):
         'Retrieves landuse at a given time'
         index = self._time.index(time)
-        return self._proportion(index)
+        return self._proportions(index)
 
     def calc_value(self, time, predictors):
        'Calculates landuse at time t using OLS, from a vector of predictors' 
        self._time.append(time)
-       self._proportion.append(dot(predictors, self._coeffs)) #TODO: Is this right?
+       self._proportions.append(dot(predictors, self._coeffs)) #TODO: Is this right?
