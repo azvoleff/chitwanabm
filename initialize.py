b2d7da7d97d058ec5140c56073139664dc436ee9
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2009-02-27 19:35:43 -0800

    Bugfixes.

--- initialize.py
+++ initialize.py
@@ -1,4 +1,4 @@
-""":
+"""
 Part of Chitwan Valley agent-based model.
 
 Sets up a CV_ABM_NS model run: Initializes neighborhood/household/person agents 
@@ -8,14 +8,17 @@ characteristics for each agent.
 Alex Zvoleff, azvoleff@mail.sdsu.edu
 """
 
+import warnings
+
 from chitwanABM import rcParams
 from chitwanABM.agents import Person, Household, Neighborhood
 
-def read_CVFS_data(textfile):
-    """Reads in CVFS data from a CSV file into a list of dictionary objects, 
-    where the first line of the file gives the column headings (used as keys 
-    within the nested dictionary object). No conversion of the fields is done: 
-    they are all stored as strings."""
+def read_CVFS_data(textfile, key_field):
+    """Reads in CVFS data from a CSV file into a dictionary of dictionary 
+    objects, where the first line of the file gives the column headings (used 
+    as keys within the nested dictionary object). No conversion of the fields 
+    is done: they are all stored as strings, EXCEPT for the key_field, which is 
+    converted and stored as an int."""
 
     try:
         file = open(textfile, 'r')
@@ -29,8 +32,7 @@ def read_CVFS_data(textfile):
     for n in xrange(len(col_names)):
         col_names[n] = col_names[n].strip('\n \"')
 
-    data = []
-
+    data = {}
     for line in lines[1:]:
         fields = line.split(',')
         for n in xrange(len(fields)):
@@ -40,7 +42,10 @@ def read_CVFS_data(textfile):
         for field, column_name in zip(fields, col_names):
             new_data[column_name] = field
 
-        data.append(new_data)
+        data_key = int(new_data[key_field])
+        if new_data.has_key(data_key):
+            raise KeyError('key %s is already in use'%(data_key))
+        data[data_key] = new_data
 
     return data
 
@@ -49,37 +54,37 @@ def assemble_neighborhoods(neighborhoodsFile):
     non-family services were available within a 30 min walk of each 
     neighborhood (SCHLFT, HLTHFT, BUSFT, MARFT, EMPFT) and on whether 
     neighborhood was electrified (ELEC)."""
-    neigh_datas = read_CVFS_data(neighborhoodsFile) 
+    neigh_datas = read_CVFS_data(neighborhoodsFile, "NEIGHID") 
 
     neighborhoods = []
-    for neigh_data in neigh_datas:
+    for neigh_data in neigh_datas.itervalues():
         NEIGHID = int(neigh_data["NEIGHID"])
         neighborhood = Neighborhood(NEIGHID, initial_agent=True)
 
         # Axinn (2007) uses the "average number of years non-family services 
         # were within a 30 minute walk" so, compute this:
         yrs_nonfamily_services = 0
-        yrs_nonfamily_services += int(neigh_data['SCHLFT']) # years schools w/in 30 min walk
-        yrs_nonfamily_services += int(neigh_data['HLTHFT']) # years health w/in 30 min walk
-        yrs_nonfamily_services += int(neigh_data['BUSFT']) # years bus w/in 30 min walk
-        yrs_nonfamily_services += int(neigh_data['MARFT']) # years market w/in 30 min walk
-        yrs_nonfamily_services += int(neigh_data['EMPFT']) # years employer w/in 30 min walk
-        neighborhood.__avg_years_nonfamily_services = yrs_nonfamily_services / 5
+        #yrs_nonfamily_services += int(neigh_data['SCHLFT']) # years schools w/in 30 min walk
+        #yrs_nonfamily_services += int(neigh_data['HLTHFT']) # years health w/in 30 min walk
+        #yrs_nonfamily_services += int(neigh_data['BUSFT']) # years bus w/in 30 min walk
+        #yrs_nonfamily_services += int(neigh_data['MARFT']) # years market w/in 30 min walk
+        #yrs_nonfamily_services += int(neigh_data['EMPFT']) # years employer w/in 30 min walk
+        #neighborhood.__avg_years_nonfamily_services = yrs_nonfamily_services / 5
 
-        neighborhood._elec_available =  bool(neigh_data['ELEC']) # is neighborhood electrified
+        #neighborhood._elec_available =  bool(neigh_data['ELEC']) # is neighborhood electrified
 
-        neighborhoods(neighborhood)
+        neighborhoods.append(neighborhood)
 
-        return neighborhoods
+    return neighborhoods
 
 def assemble_households(householdsFile):
     """Reads in data from the CVFS (from dataset DS0002) on several statistics 
     for each household (BAA43, BAA44, BAA10A, BAA18A)."""
-    household_datas = read_CVFS_data(householdsFile) 
+    household_datas = read_CVFS_data(householdsFile, "HHID")
     
     households = []
     HHID_NEIGHID_map = {} # Links persons with their HHID
-    for household_data in household_datas:
+    for household_data in household_datas.itervalues():
         HHID = int(household_data['HHID'])
         NEIGHID = int(household_data['NEIGHID'])
         HHID_NEIGHID_map[HHID] = NEIGHID
@@ -107,13 +112,13 @@ def assemble_persons(relationshipsFile, censusFile):
     DS0004 (restricted)) and from the household relationship grid, CVFS 
     dataset DS0015 (public) or DS0016 (restricted), and assembles person, 
     household, neighborhood, and region agents."""
-    relations = read_CVFS_data(relationshipsFile) 
-    census = read_CVFS_data(censusFile) 
+    relations = read_CVFS_data(relationshipsFile, "RESPID") 
+    census = read_CVFS_data(censusFile, "RESPID") 
 
     # For each household, create a SUBJECT -> RESPID mapping.  For example:
     # SUBJECT_RESPID_map[HHID][SUBJECT] = RESPID
     SUBJECT_RESPID_map = {}
-    for relation in relations:
+    for relation in relations.itervalues():
         RESPID = int(relation['RESPID'])
         SUBJECT = int(relation['SUBJECT'])
         HHID = int(relation['HHID'])
@@ -126,27 +131,52 @@ def assemble_persons(relationshipsFile, censusFile):
     # Loop over all agents in the relationship grid.
     persons = []
     RESPID_HHID_map = {} # Links persons with their HHID
-    for relation in persons:
+    for relation in relations.itervalues():
         RESPID = int(relation['RESPID'])
         HHID = int(relation['HHID'])
         RESPID_HHID_map[RESPID] = HHID
 
         # Get the agent's sex and age from the census data
-        CENAGE = int(census[RESPID]['CENAGE'])
-        CENGENDR = census[RESPID]['CENGENDR']
+        try:
+            CENAGE = int(census[RESPID]['CENAGE'])
+            CENGENDR = census[RESPID]['CENGENDR']
+        except KeyError:
+            print "WARNING: no census data on person %s. This agent will be excluded from the model."%(RESPID)
+            continue
+        except ValueError:
+            print "WARNING: no census data on person %s. This agent will be excluded from the model."%(RESPID)
+            continue
         
-        # Read in SUBJECT RESPIDs of parents/spouse/children
-        mother_SUBJECT = int(relations[RESPID]['PARENT1'])
-        father_SUBJECT = int(relations[RESPID]['PARENT2'])
-        spouse_SUBJECT = int(relations[RESPID]['SPOUSE1'])
-
+        # Read in SUBJECT IDs of parents/spouse/children
+        mother_SUBJECT = int(relation['PARENT1'])
+        father_SUBJECT = int(relation['PARENT2'])
+        spouse_SUBJECT = int(relation['SPOUSE1'])
 
         # Convert SUBJECT IDs into RESPIDs
-        father_RESPID = SUBJECT_RESPID_map[HHID][father_SUBJECT]
-        mother_RESPID = SUBJECT_RESPID_map[HHID][mother_SUBJECT]
-        spouse_RESPID = SUBJECT_RESPID_map[HHID][spouse_SUBJECT]
+        if father_SUBJECT != 0:
+            father_RESPID = SUBJECT_RESPID_map[HHID][father_SUBJECT]
+        else:
+            father_RESPID = None
 
-        person = Person(None, RESPID, mother_RESPID, father_RESPID, CENAGE, CENGENDR, initial_agent=True)
+        if mother_SUBJECT != 0:
+            mother_RESPID = SUBJECT_RESPID_map[HHID][mother_SUBJECT]
+        else:
+            mother_RESPID = None
+
+        if spouse_SUBJECT != 0:
+            spouse_RESPID = SUBJECT_RESPID_map[HHID][spouse_SUBJECT]
+        else:
+            spouse_RESPID = None
+
+        # Convert numerical genders to "male" or "female". 1 = male, 2 = female
+        if CENGENDR == '1':
+            CENGENDR = "male"
+        elif CENGENDR == '2':
+            CENGENDR = "female"
+
+        # Finally, make the new person.
+        person = Person(None, RESPID, mother_RESPID, father_RESPID, CENAGE, 
+                CENGENDR, initial_agent=True)
 
         persons.append(person)
 
@@ -166,7 +196,7 @@ def assemble_region(region):
 
     # Populate the region
     for neighborhood in neighborhoods:
-        Region.add_agent(neighborhood)
+        region.add_agent(neighborhood)
 
     # Now populate the neighborhoods with households. For this we need the HHID 
     # -> NID mapping:
@@ -174,7 +204,7 @@ def assemble_region(region):
         HHID = household.get_ID()
         NEIGHID = HHID_NEIGHID_map[HHID]
         # Get a reference to this neighborhood, and add the household
-        neighborhood = Region.get_agent(NEIGHID)
+        neighborhood = region.get_agent(NEIGHID)
         neighborhood.add_agent(household)
 
     # Now populate the households with people. For this we need the RESPID -> 
@@ -182,11 +212,19 @@ def assemble_region(region):
     for person in persons:
         RESPID = person.get_ID()
         HHID = RESPID_HHID_map[RESPID]
-        NEIGHID = HHID_NEIGHID_map[HHID]
+        try:
+            NEIGHID = HHID_NEIGHID_map[HHID]
+        except KeyError:
+            print "WARNING: household %s is not in DS0002. This agent will be excluded from the model."%(HHID)
+            continue
         # Get a reference to this neighborhood
-        neighborhood = Region.get_agent(NEIGHID)
+        neighborhood = region.get_agent(NEIGHID)
         # Then get a reference to the proper household, and add the person
         household = neighborhood.get_agent(HHID)
         household.add_agent(person)
+
+    print "Num persons:", str(len(persons))
+    print "Num households:", str(len(households))
+    print "Num neighborhoods:", str(len(neighborhoods))
 
     return region
