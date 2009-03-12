0949561a6f783b432bcda9ff38b218afaccf5ea4
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2009-03-12 15:29:02 -0700

    Whitespace.

--- processing/plotBasicStats.py
+++ processing/plotBasicStats.py
@@ -2,8 +2,8 @@
 """
 Part of Chitwan Valley agent-based model.
 
-Plot basic statistics on a model run: From a set of model output files, plots 
-basic statistics summarizing the model run.
+Plot basic statistics on a model run: From a set of model output files, 
+plots basic statistics summarizing the model run.
 
 Alex Zvoleff, azvoleff@mail.sdsu.edu
 """
