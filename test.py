e6c17ef4c842fd351310a8ab5f9102e8728ea214
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2009-06-17 11:33:21 -0700

    Add the skeleton of a test script.
    Still need to write the tests for validation of the model code.

---
+++ test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+"""
+Part of Chitwan Valley agent-based model.
+
+Script to run a test of the Chitwan Valley ABM code, to ensure, for example, 
+that agents are properly initialized, and that demographic processes 
+(marriages, deaths, births, migrations) are all properly represented in the 
+model.
+
+Alex Zvoleff, azvoleff@mail.sdsu.edu
+"""
+###############################################################################
+# Check initialization from CVFS data
+###############################################################################
+# Ensure init_data.pickle is the most recent version
+
+# Ensure relationships are tracked properly
+
+###############################################################################
+# Double check demographic processes
+###############################################################################
+# Ensure marriage works correctly
+
+# Ensure deaths work correctly
+
+# Ensure births work properly
+
+# Ensure migration works properly
+
+###############################################################################
+# Check that hazards are used correctly
+###############################################################################
+# Ensure hazards are read in properly
+
+# Ensure hazards are indexed properly
