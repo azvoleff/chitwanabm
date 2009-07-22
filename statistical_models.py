85bbd2f76d7c04dc9491f9e3df2841f5804f2895
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2009-07-22 09:44:40 -0700

    Improve efficiency of hazard unit conversion.

--- statistical_models.py
+++ statistical_models.py
@@ -20,11 +20,32 @@ if rcParams['model.use_psyco'] == True:
 
 hazard_time_units = rcParams['hazard.time_units']
 
+def convert_hazard_units(hazard):
+    """
+    Converts hazard so units match timestep used in the model, assuming hazard 
+    function is uniform across the interval.
+
+    Conversions are made accordingly using conditional probability.
+    """
+    # If the hazard time units don't match the model timestep units, then the 
+    # hazards need to be converted.
+    if hazard_time_units == 'months':
+        pass
+    elif hazard_time_units == 'years':
+        for key, value in hazard.iteritems():
+            hazard[key] = 1 - (1 - value)**(1/12.)
+    elif hazard_time_units == 'decades':
+        for key, value in hazard.iteritems():
+            hazard[key] = 1 - (1 - value)**(1/120.)
+    else:
+        raise UnitsError("unhandled hazard_time_units")
+    return hazard
+
 #TODO: these hazards should be derived from the region, not directly from RcParams
-birth_hazards = rcParams['hazard.birth']
-death_hazards = rcParams['hazard.death']
-marriage_hazards = rcParams['hazard.marriage']
-migration_hazards = rcParams['hazard.migration']
+birth_hazards = convert_hazard_units(rcParams['hazard.birth'])
+death_hazards = convert_hazard_units(rcParams['hazard.death'])
+marriage_hazards = convert_hazard_units(rcParams['hazard.marriage'])
+migration_hazards = convert_hazard_units(rcParams['hazard.migration'])
 
 class UnitsError(Exception):
     pass
@@ -46,24 +67,6 @@ def __hazard_index__(t):
     else:
         raise UnitsError("unhandled hazard_time_units")
 
-def convert_hazard_units(hazard):
-    """
-    Converts hazard so units match timestep used in the model, assuming hazard 
-    function is uniform across the interval.
-
-    Conversions are made accordingly using conditional probability.
-    """
-    # If the hazard time units don't match the model timestep units, then the 
-    # hazards need to be converted.
-    if hazard_time_units == 'months':
-        return hazard
-    elif hazard_time_units == 'years':
-        return 1 - (1 - hazard)**(1/12.)
-    elif hazard_time_units == 'decades':
-        return 1 - (1 - hazard)**(1/120.)
-    else:
-        raise UnitsError("unhandled hazard_time_units")
-
 #def hazard_birth(person, neighborhood, landuse):
 def calc_hazard_birth(person):
     "Calculates the hazard of birth for an agent."
@@ -72,21 +75,21 @@ def calc_hazard_birth(person):
     #hazard_multiplier = 1 + (person.get_parent_agent().num_members()/30.)
     #hazard_multiplier = 1 - (person.get_parent_agent().num_members()/25.)
     hazard_multiplier = 1
-    return hazard_multiplier * convert_hazard_units(birth_hazards[hazard_index])
+    return hazard_multiplier * birth_hazards[hazard_index]
 
 #def hazard_marriage(person, neighborhood, landuse):
 def calc_hazard_marriage(person):
     "Calculates the hazard of marriage for an agent."
     age = person.get_age()
     hazard_index = __hazard_index__(age)
-    return convert_hazard_units(marriage_hazards[hazard_index])
+    return marriage_hazards[hazard_index]
 
 def calc_hazard_death(person):
     "Calculates the hazard of death for an agent."
     age = person.get_age()
     hazard_index = __hazard_index__(age)
     try:
-        return convert_hazard_units(death_hazards[hazard_index])
+        return death_hazards[hazard_index]
     except IndexError:
         raise IndexError("error calculating death hazard (index %s)"%(hazard_index))
 
@@ -94,7 +97,7 @@ def calc_hazard_migration(person):
     "Calculates the hazard of death for an agent."
     age = person.get_age()
     hazard_index = __hazard_index__(age)
-    return convert_hazard_units(migration_hazards[hazard_index])
+    return migration_hazards[hazard_index]
 
 def calc_landuse(region):
     "Calculates land use based on population parameters and past land use."
