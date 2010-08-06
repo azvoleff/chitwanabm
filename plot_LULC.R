1356fdea050c58296a6aa5682c36aad322b5f764
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2010-08-06 11:24:57 -0700

    Make R plot scripts use calc_NBH_stats.

--- plot_LULC.R
+++ plot_LULC.R
@@ -2,44 +2,20 @@
 # Plots the LULC data from a model run.
 require(ggplot2)
 
-#MODEL_RUN_ID <- "20100803-161842"
-#DATA_PATH <- paste("~/Data/ChitwanABM_runs", MODEL_RUN_ID, sep="/")
+source("calc_NBH_stats.R")
+
 DATA_PATH <- commandArgs(trailingOnly=TRUE)[1]
 
-theme_update(theme_grey(base_size=18))
-update_geom_defaults("line", aes(size=1))
-
-# Make plots of LULC for a model run.
-lulc <- read.csv(paste(DATA_PATH, "LULC_results.csv", sep="/"))
-time.values <- read.csv(paste(DATA_PATH, "time.csv", sep="/"))
-time.Robj <- as.Date(paste(time.values$time_date, "15", sep=","),
-        format="%m/%Y,%d")
-time.values <- cbind(time.values, time.Robj=time.Robj)
-
-agveg.cols <- grep('^agveg.[0-9]*$', names(lulc))
-nonagveg.cols <- grep('^nonagveg.[0-9]*$', names(lulc))
-pubbldg.cols <- grep('^pubbldg.[0-9]*$', names(lulc))
-privbldg.cols <- grep('^privbldg.[0-9]*$', names(lulc))
-other.cols <- grep('^other.[0-9]*$', names(lulc))
-
-# Calculate the total land area of each neighborhood
-nbh.area <- apply(cbind(lulc$agveg.1, lulc$nonagveg.1, lulc$pubbldg.1,
-        lulc$privbldg.1, lulc$other.1), 1, sum)
-
-# And convert the LULC measurements from units of square meters to units that 
-# are a percentage of total neighborhood area.
-lulc.sd <- lulc/nbh.area
-
-lulc.sd.mean <- data.frame(time.Robj=time.Robj,
-        agveg=apply(lulc.sd[agveg.cols], 2, mean),
-        nonagveg=apply(lulc.sd[nonagveg.cols], 2, mean),
-        pubbldg=apply(lulc.sd[pubbldg.cols], 2, mean),
-        privbldg=apply(lulc.sd[privbldg.cols], 2, mean),
-        other=apply(lulc.sd[other.cols], 2, mean), row.names=NULL)
+lulc.sd.mean <- calc_NBH_LULC(DATA_PATH)
 # Stack lulc.mean so it can easily be used with ggplot2 faceting
+time.Robj <- lulc.sd.mean$time.Robj
 lulc.sd.mean <- stack(lulc.sd.mean)
 lulc.sd.mean <- cbind(time.Robj=rep(time.Robj,5), lulc.sd.mean)
 names(lulc.sd.mean)[2:3] <- c("area", "LULC_type")
+
+# Now actually make the plots
+theme_update(theme_grey(base_size=18))
+update_geom_defaults("line", aes(size=1))
 
 qplot(time.Robj, area, geom="line", colour=LULC_type, xlab="Year",
         ylab="Mean Percentage of Neighborhood", data=lulc.sd.mean)
--- plot_pop.R
+++ plot_pop.R
@@ -2,78 +2,50 @@
 # Plots the pop data from a model run.
 require(ggplot2)
 
+source("calc_NBH_stats.R")
+
 DATA_PATH <- commandArgs(trailingOnly=TRUE)[1]
 #MODEL_RUN_ID <- '20100804-204228'
 #DATA_PATH <- paste("~/Data/ChitwanABM_runs", MODEL_RUN_ID, sep="/")
 
-theme_update(theme_grey(base_size=18))
-update_geom_defaults("line", aes(size=1))
+pop.results <- calc_NBH_pop(DATA_PATH)
 
-pop.results <- read.csv(paste(DATA_PATH, "pop_results.csv", sep="/"))
-
-# Read in time data to use in plotting. time.Robj will provide the x-axis 
-# values.
-time.values <- read.csv(paste(DATA_PATH, "time.csv", sep="/"))
-time.Robj <- as.Date(paste(time.values$time_date, "15", sep=","),
-        format="%m/%Y,%d")
-time.values <- cbind(time.values, time.Robj=time.Robj)
-
-num_psn.cols <- grep('^num_psn.[0-9]*$', names(pop.results))
-num_hs.cols <- grep('^num_hs.[0-9]*$', names(pop.results))
-# num_marr is total number of marriages in the neighborhood, whereas marr is 
-# the number of new marriages in a particular month.
-num_marr.cols <- grep('^num_marr.[0-9]*$', names(pop.results))
-marr.cols <- grep('^marr.[0-9]*$', names(pop.results))
-births.cols <- grep('^births.[0-9]*$', names(pop.results))
-deaths.cols <- grep('^deaths.[0-9]*$', names(pop.results))
-in_migr.cols <- grep('^in_migr.[0-9]*$', names(pop.results))
-out_migr.cols <- grep('^out_migr.[0-9]*$', names(pop.results))
-fw_usage.cols <- grep('^fw_usage.[0-9]*$', names(pop.results))
-
-# Make two separate stacks - one of pop data, and one of event data.
-# Stack them so they can easily be used with ggplot2 faceting.
-events <- data.frame(time.Robj=time.Robj,
-        marr=apply(pop.results[marr.cols], 2, sum, na.rm=TRUE), 
-        births=apply(pop.results[births.cols], 2, sum, na.rm=TRUE), 
-        deaths=apply(pop.results[deaths.cols], 2, sum, na.rm=TRUE), 
-        in_migr=apply(pop.results[in_migr.cols], 2, sum, na.rm=TRUE), 
-        out_migr=apply(pop.results[out_migr.cols], 2, sum, na.rm=TRUE))
+# Make two separate stacks - one of monthly event data and one of total hs and 
+# total marriages. Stack them so they can easily be color-coded with ggplot2.
+events <- with(pop.results, data.frame(time.Robj=time.Robj, marr, births, deaths))
 events <- stack(events)
-events <- cbind(time.Robj=rep(time.Robj,5), events)
+events <- cbind(time.Robj=rep(pop.results$time.Robj, 3), events)
 names(events)[2:3] <- c("events", "Event_type")
 
+num.hs.marr <- with(pop.results, data.frame(time.Robj=time.Robj, num_hs, num_marr))
+num.hs.marr <- stack(num.hs.marr)
+num.hs.marr <- cbind(time.Robj=rep(pop.results$time.Robj,2), num.hs.marr)
+names(num.hs.marr)[2:3] <- c("num", "Pop_type")
+
+# First plot monthly event data
+theme_update(theme_grey(base_size=18))
 # Plot thinner lines so this busy plot is easier to read.
 update_geom_defaults("line", aes(size=.5))
-qplot(time.Robj, events, geom="line", colour=Event_type, xlab="Year",
+qplot(pop.results$time.Robj, events, geom="line", colour=Event_type, xlab="Year",
         ylab="Number of Events", data=events)
 ggsave(paste(DATA_PATH, "pop_events.png", sep="/"), width=8.33, height=5.53,
         dpi=300)
 
-update_geom_defaults("line", aes(size=1))
-num.psn <- data.frame(time.Robj=time.Robj,
-        num.psn=apply(pop.results[num_psn.cols], 2, sum, na.rm=TRUE))
-qplot(time.Robj, num.psn, geom="line", xlab="Year",
-        ylab="Population", data=num.psn)
-ggsave(paste(DATA_PATH, "pop_num_psn.png", sep="/"), width=8.33, height=5.53,
-        dpi=300)
-
-num.other <- data.frame(time.Robj=time.Robj,
-        num.hs=apply(pop.results[num_hs.cols], 2, sum, na.rm=TRUE), 
-        num.marr=apply(pop.results[num_marr.cols], 2, sum, na.rm=TRUE))
-num.other <- stack(num.other)
-num.other <- cbind(time.Robj=rep(time.Robj,2), num.other)
-names(num.other)[2:3] <- c("num", "Pop_type")
+# Now plot total households and total marriages
 qplot(time.Robj, num, geom="line", colour=Pop_type, xlab="Year",
-        ylab="Population", data=num.other)
+        ylab="Population", data=num.hs.marr)
 ggsave(paste(DATA_PATH, "pop_num_hs_marr.png", sep="/"), width=8.33, height=5.53,
         dpi=300)
 
-fw.usage <- data.frame(time.Robj=time.Robj,
-        fw_usage=apply(pop.results[fw_usage.cols], 2, sum, na.rm=TRUE))
-fw.usage <- stack(fw.usage)
-fw.usage <- cbind(time.Robj=rep(time.Robj,2), fw.usage)
-names(fw.usage)[2:3] <- c("kg_fw", "Pop_type")
-qplot(time.Robj, kg_fw/1000, geom="line", colour=Pop_type, xlab="Year",
-        ylab="Metric Tons of Fuelwood", data=fw.usage)
+# Plot total population
+update_geom_defaults("line", aes(size=1))
+qplot(time.Robj, num_psn, geom="line", xlab="Year",
+        ylab="Population", data=pop.results)
+ggsave(paste(DATA_PATH, "pop_num_psn.png", sep="/"), width=8.33, height=5.53,
+        dpi=300)
+
+# Plot fw consumption in metric tons
+qplot(time.Robj, fw_usage_kg/1000, geom="line", xlab="Year",
+        ylab="Metric Tons of Fuelwood", data=pop.results)
 ggsave(paste(DATA_PATH, "fw_usage.png", sep="/"), width=8.33, height=5.53,
         dpi=300)
