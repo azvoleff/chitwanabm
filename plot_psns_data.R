efa4fb7f2a1dd20780f63ee59d954d8c2ae4c92c
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2010-08-10 18:27:20 -0400

    Also plot hhsize.

--- plot_psns_data.R
+++ plot_psns_data.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 # Plots the individual person data from a model run.
 require(epicalc)
+require(ggplot2, quietly=TRUE)
 
 DATA_PATH <- commandArgs(trailingOnly=TRUE)[1]
 
@@ -13,12 +14,27 @@ files <- files[grep("^psns_time_[0-9]*.txt$", files)]
 files <- files[order(as.numeric(gsub("(^psns_time_)|(.txt$)", "", files)))]
 pdf_file_path <- paste(DATA_PATH, "/psns_pop_pyramid.pdf", sep="") 
 pdf(file=pdf_file_path)
+hhsize <- c()
+timesteps <-c()
 for (psns_file in files) {
     timestep <- gsub("(^psns_time_)|(.txt$)", "",  psns_file)
+    timesteps <- c(timesteps, timestep)
     full_file_path <- paste(DATA_PATH, "/", psns_file, sep="") 
     psns_data <- read.csv(full_file_path)
     # Cover ages from months to years
     psns_data$age <- psns_data$age/12
     pyramid(psns_data$age, psns_data$gender, main=paste("Timestep", timestep, sep=" "))
+    hhsize <- c(hhsize, mean(table(psns_data$hid)))
 }
 dev.off()
+
+time.values <- read.csv(paste(DATA_PATH, "time.csv", sep="/"))
+time.Robj <- as.Date(paste(time.values$time_date, "15", sep=","),
+        format="%m/%Y,%d")
+time.values <- cbind(time.values, time.Robj=time.Robj)
+time.Robj <- time.values$time.Robj[time.values$timestep %in% timesteps]
+
+qplot(time.Robj, hhsize, geom="line", xlab="Year",
+        ylab="Mean Household Size (number of persons)")
+ggsave(paste(DATA_PATH, "hhsize.png", sep="/"), width=8.33, height=5.53,
+        dpi=300)
