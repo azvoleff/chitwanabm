630a75b6e84f52a95b9e6533c565e6f8fba36623
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2012-02-07 22:07:46 -0800

    Update to use new PyABM 'utility' script.

--- modelloop.py
+++ modelloop.py
@@ -32,92 +32,12 @@ import copy
 import numpy as np
 
 from PyABM import file_io
+from PyABM.utility import TimeSteps
 from ChitwanABM import rcParams
 
 if rcParams['model.use_psyco'] == True:
     import psyco
     psyco.full()
-
-class TimeSteps():
-    def __init__(self, bounds, timestep):
-        self._starttime = bounds[0]
-        self._endtime = bounds[1]
-        self._timestep = timestep
-
-        # Initialize the current month and year
-        self._year = self._starttime[0]
-        self._month = self._starttime[1]
-        self._int_timestep = 1
-
-    def increment(self):
-        self._month += self._timestep
-        dyear = int((self._month - 1) / 12)
-        self._year += dyear
-        self._month = self._month - dyear*12
-        self._int_timestep += 1
-        assert self._month != 0, "Month cannot be 0"
-
-    def in_bounds(self):
-        if self._year == self._endtime[0] and self._month >= self._endtime[1] \
-                or self._year > self._endtime[0]:
-            return False
-        else:
-            return True
-
-    def is_last_iteration(self):
-        next_month = self._month + self._timestep
-        dyear = int((next_month - 1) / 12)
-        next_year = self._year + dyear
-        next_month = next_month - dyear*12
-        if next_year >= self._endtime[0] and next_month >= self._endtime[1]:
-            return True
-        else:
-            return False
-    
-    def get_cur_month(self):
-        return self._month
-
-    def get_cur_year(self):
-        return self._year
-
-    def get_cur_date(self):
-        return [self._year, self._month]
-
-    def get_T0_date(self):
-        """
-        Returns the time one timestep prior to the starting time of the model 
-        (T0).
-        """
-        T0_month = self._month - self._timestep
-        dyear = int(1 - np.ceil(T0_month / 12.))
-        T0_year = self._year - dyear
-        T0_month = T0_month + dyear*12
-        return [T0_year, T0_month]
-
-    def get_cur_date_string(self):
-        return "%.2d/%s"%(self._month, self._year)
-
-    def get_T0_date_string(self):
-        T0_year, T0_month = self.get_T0_date()
-        return "%.2d/%s"%(T0_month, T0_year)
-
-    def get_cur_date_float(self):
-        return self._year + (self._month-1)/12.
-
-    def get_T0_date_float(self):
-        """
-        Returns the time float one timestep prior to the starting time of the 
-        model (T0).
-        """
-        T0_year, T0_month = self.get_T0_date()
-        return T0_year + (T0_month-1)/12.
-
-    def get_cur_int_timestep(self):
-        return self._int_timestep
-
-    def __str__(self):
-        return "%s-%s"%(self._year, self._month)
-
 
 timebounds = rcParams['model.timebounds']
 timestep = rcParams['model.timestep']
