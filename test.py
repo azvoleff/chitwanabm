39f3aedc09da1d817e01775b72670d088b976fc9
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2012-07-27 18:52:33 -0700

    Add more test cases.

--- test.py
+++ test.py
@@ -37,6 +37,14 @@ logger = logging.getLogger(__name__)
 import numpy as np
 
 def validate_person_attributes(world):
+    def get_person_info(person):
+        person_info ="(age: %.2f, ethnicity: %s, in-mig: %s, initial: %s, HH: %s, NBH: %s, stores: %s, alive: %s)"%( \
+                person.get_age_years(), person.get_ethnicity(), 
+                person.is_in_migrant(), person.is_initial_agent(), 
+                person.get_parent_agent().get_ID(),
+                person.get_parent_agent().get_parent_agent().get_ID(), 
+                person._store_list, person._alive)
+        return person_info
     logger.debug("Validating person attributes")
     all_agents_valid = True
     valid_ethnicities = ["HighHindu",
@@ -45,49 +53,52 @@ def validate_person_attributes(world):
                          "Newar",
                          "TeraiTibeto"]
     maximum_age = 110
-    for region in world.get_regions():
-        for person in region.iter_persons():
-            person_info ="(age: %.2f, ethnicity: %s, in-mig: %s, initial: %s, HH: %s, NBH: %s)"%( \
-                person.get_age_years(), person.get_ethnicity(), 
-                person.is_in_migrant(), person.is_initial_agent(), 
-                person.get_parent_agent().get_ID(),
-                person.get_parent_agent().get_parent_agent().get_ID())
-
-            if person.get_age_months() != np.round(person.get_age_years() * 12):
-                logger.warning("Person %s age in months (%.2f) does not match age in years (%.2f) %s"%(
-                    person.get_ID(), person.get_age_months(), person.get_age_years(), person_info))
-                all_agents_valid = False
-            if person.get_age_years() > maximum_age:
-                logger.warning("Person %s is older than the maximum allowed age (%s) %s"%(
-                    person.get_ID(), maximum_age, person.get_age_years(), person_info))
-                all_agents_valid = False
-            if person.get_ethnicity() not in valid_ethnicities:
-                logger.warning("Person %s is not a valid ethnicity %s"%(person.get_ID(), person_info))
-                all_agents_valid = False
-            if not person._alive:
-                logger.warning("Person %s %s is dead, but is still participating in household activities"%(
-                    person.get_ID(), person_info))
-            if person.get_age_years() < 0 or person.get_age_months() < 0:
-                logger.warning("Person %s %s has not been born yet, but is already participating in household activities"%(
-                    person.get_ID(), person_info))
-            if person.get_spouse() != None and not person.get_spouse()._alive:
-                logger.warning("Spouse of person %s (spouse ID %s) is dead %s"%(
-                    person.get_ID(), person.get_spouse().get_ID(), person_info))
-                all_agents_valid = False
-            if person.get_parent_agent() == None:
-                logger.warning("Person %s %s is not a member of any household"%(
-                    person.get_ID(), person_info))
-                all_agents_valid = False
+    for person in world.iter_persons():
+        if person.get_parent_agent() == None:
+            logger.warning("Person %s %s is not a member of any household"%(
+                person.get_ID(), person_info))
+            all_agents_valid = False
+        person_info = get_person_info(person)
+        if person.get_age_months() != np.round(person.get_age_years() * 12):
+            logger.warning("Person %s age in months (%.2f) does not match age in years (%.2f) %s"%(
+                person.get_ID(), person.get_age_months(), person.get_age_years(), person_info))
+            all_agents_valid = False
+        if person.get_age_years() > maximum_age:
+            logger.warning("Person %s is older than the maximum allowed age (%s) %s"%(
+                person.get_ID(), maximum_age, person.get_age_years(), person_info))
+            all_agents_valid = False
+        if person.get_ethnicity() not in valid_ethnicities:
+            logger.warning("Person %s is not a valid ethnicity %s"%(person.get_ID(), person_info))
+            all_agents_valid = False
+        if not person._alive:
+            logger.warning("Person %s %s is dead, but is still participating in household activities"%(
+                person.get_ID(), person_info))
+            all_agents_valid = False
+        if person.get_age_years() < 0 or person.get_age_months() < 0:
+            logger.warning("Person %s %s has not been born yet, but is already participating in household activities"%(
+                person.get_ID(), person_info))
+            all_agents_valid = False
+        if person.get_spouse() != None and not person.get_spouse()._alive:
+            spouse_info = get_person_info(person.get_spouse())
+            logger.warning("Spouse of person %s (spouse ID %s) is dead. Person: %s, Spouse: %s"%(
+                person.get_ID(), person.get_spouse().get_ID(), person_info, spouse_info))
+            all_agents_valid = False
     return all_agents_valid
 
 def validate_household_attributes(world):
     logger.debug("Validating household attributes")
     # TODO: Complete validation function.
     all_agents_valid = True
+    for region in world.iter_regions():
+        for household in region.iter_households():
+            pass
     return all_agents_valid
 
 def validate_neighborhood_attributes(world):
     logger.debug("Validating neighborhood attributes")
+    for region in world.iter_regions():
+        for neighborhood in region.iter_agents():
+            pass
     # TODO: Complete validation function.
     all_agents_valid = True
     return all_agents_valid
