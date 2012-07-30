d51ddaa8c50e817df4c20a96c93f979416e05c7a
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2012-07-30 08:36:34 -0700

    Use semaphore in threaded batch script.

--- threaded_batch_run.py
+++ threaded_batch_run.py
@@ -5,15 +5,18 @@ import os
 import threading
 import subprocess
 
-number_of_runs = 20
-max_threads = 3
+number_of_runs = 10
+max_threads = 2
 
 time_format = "%m/%d/%Y %I:%M:%S %p"
+
+pool_sema = threading.BoundedSemaphore(value=(max_threads+1))
 
 python_path = "C:/Python27/python.exe"
 script_path = "C:/Users/azvoleff/Code/Python/ChitwanABM/runmodel.py"
 class ChitwanABMThread(threading.Thread):
     def __init__(self, thread_ID):
+        pool_sema.acquire()
         threading.Thread.__init__(self)
         self.threadID = thread_ID
         self.name = thread_ID
@@ -23,17 +26,14 @@ class ChitwanABMThread(threading.Thread):
         dev_null.close()
         end_time = time.strftime(time_format, time.localtime())
         print "%s: Finished run %s"%(end_time, self.name)
+        pool_sema.release()
 
 run_count = 1
 while run_count <= number_of_runs:
-    if run_count != 1:
-        current_time = time.strftime(time_format, time.localtime())
-        print "%s: Waiting to start new thread"%(current_time)
-    while threading.active_count() <= max_threads:
+    with pool_sema:
         new_thread = ChitwanABMThread(run_count)
         start_time = time.strftime(time_format, time.localtime())
         print "%s: Starting run %s"%(start_time, new_thread.name)
         new_thread.start()
-        time.sleep(20)
+        time.sleep(5)
         run_count += 1
-    time.sleep(60)
