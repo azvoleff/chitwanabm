eb72d4771dd815004148772321b839d5d26c45d3
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2012-08-23 11:44:00 -0700

    Add SDSU lab convenience script.

---
+++ misc/SDSU_runmodel.py
@@ -0,0 +1,25 @@
+"""
+Script used to simplify running the ChitwanABM on SDSU lab computers. Sets up 
+the python and system paths so the model can find all the required 
+dependencies.
+"""
+
+import os
+import sys
+
+def main():
+    # First setup system path
+    os.environ['PATH'] = "Z:\\Programs\GDAL;Z:\\Python_Local_64bit\Scripts;" + os.environ['PATH']
+
+    # Now setup python path
+    oldpaths = sys.path
+    sys.path = ["Z:\\Python_Local_64bit\site-packages", "Z:\\Code", 
+                "Z:\\Python_Local_64bit\Scripts"]
+    sys.path.extend(oldpaths)
+
+    import ChitwanABM.runmodel
+
+    ChitwanABM.runmodel.main()
+
+if __name__ == "__main__":
+    sys.exit(main())
