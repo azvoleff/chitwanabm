c5b77d56ddb9919ecbc8c019589b5907fe82e227
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2012-09-04 09:37:40 -0700

    Move misc inside main chitwanabm folder.

---
+++ chitwanabm/misc/__init__.py
@@ -0,0 +1,24 @@
+# Copyright 2008-2012 Alex Zvoleff
+#
+# This file is part of the chitwanabm agent-based model.
+# 
+# chitwanabm is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+# 
+# chitwanabm is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# chitwanabm.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contact Alex Zvoleff (azvoleff@mail.sdsu.edu) in the Department of Geography 
+# at San Diego State University with any comments or questions. See the 
+# README.txt file for contact information.
+"""
+The misc folder contains code for running the chitwanabm in a lab setting where 
+users lack the necessary permissions to manipulate the path and/or to install 
+python packages.
+"""
