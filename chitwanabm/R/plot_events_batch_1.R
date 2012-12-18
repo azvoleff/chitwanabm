7f4d1e4a0c759f9802cadef0fd1e52c922c0ae5c
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2012-12-18 15:32:35 -0800

    Add new event plot code - rename script later.

---
+++ chitwanabm/R/plot_events_batch_1.R
@@ -0,0 +1,174 @@
+#!/usr/bin/env Rscript
+#
+# Copyright 2008-2012 Alex Zvoleff
+#
+# This file is part of the chitwanabm agent-based model.
+# 
+# chitwanabm is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+# 
+# chitwanabm is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# chitwanabm.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contact Alex Zvoleff in the Department of Geography at San Diego State 
+# University with any comments or questions. See the README.txt file for 
+# contact information.
+
+library(ggplot2, quietly=TRUE)
+library(reshape)
+
+PLOT_WIDTH = 8.33
+PLOT_HEIGHT = 5.53
+theme_set(theme_grey(base_size=16))
+update_geom_defaults("line", aes(size=1))
+
+DATA_PATH <- commandArgs(trailingOnly=TRUE)[1]
+DATA_PATH <- "G:/Data/Nepal/chitwanabm_runs/Double_Feedbacks"
+DATA_PATH <- "G:/Data/Nepal/chitwanabm_runs/No_Feedbacks"
+
+calc_event_data <- function(event_type, run_path) {
+    events_data <- read.csv(paste(run_path, "person_events.log", sep="/"), na.strings=c("NA", "None"))
+    events <- events_data[grep('^(nid|age|time|event|marrtime)$', names(events_data))]
+    events <- events[events$event %in% event_type, ]
+    # Below line is a kludge to work around times being stored in the results 
+    # as floats - they should really be stored as timesteps. TODO: Fix this in 
+    # the agent.py file.
+    events$year <- floor(events$time)
+    events$fb_int <- (events$time - events$marrtime) * 12
+    return(events)
+}
+
+directories <- list.dirs(DATA_PATH, recursive=FALSE)
+# Only match the model results folders - don't match any other folders or files 
+# in the directory, as trying to read results from these other files/folders 
+# would lead to an error.
+directories <- directories[grep("[0-9]{8}-[0-9]{6}", directories)]
+if (length(directories)<1) stop(paste("can't run plot_events_batch with", length(directories), "model runs."))
+if (length(directories)<5) warning(paste("Only", length(directories), "model runs found."))
+
+time_values <- read.csv(paste(directories[1], "time.csv", sep="/"))
+time.Robj <- as.Date(paste(time_values$time_date, "15", sep=","),
+        format="%m/%Y,%d")
+time_values <- cbind(time_values, time.Robj=time.Robj)
+time_values$year <- floor(time_values$time_float)
+
+event_type <- c("First birth", "Marriage")
+n <- 1
+for (directory in directories) {
+    possible_error <- tryCatch(events.new <- calc_event_data(event_type, directory), 
+                               error=function(e) e)
+    # Catch cases where results file does not exist (because run was 
+    # interrupted, run is not yet finished, etc.)
+    if (inherits(possible_error, "error")) {
+        warning(paste("Error reading", directory))
+        next
+    }
+    runname <- paste("run", n, sep="")
+    events.new <- cbind(events.new, runname=rep(runname, nrow(events.new)))
+    if (n==1) {events <- events.new}
+    else {events <- rbind(events, events.new)}
+    n <- n + 1
+}
+
+# Merge the initial cover types so results can be plotted by cover class
+results <- read.csv(paste(directories[1], "run_results.csv", sep="/"))
+nbh.area <- apply(cbind(results$agveg.1, results$nonagveg.1, results$pubbldg.1,
+            results$privbldg.1, results$other.1), 1, sum)
+results$pctagveg.1 <- (results$agveg.1 / nbh.area) * 100
+results$lctype <- cut(results$pctagveg.1, c(-.01, 25, 50, 75, 100), 
+                      labels=c('Urban', 'Mixed Urban', 'Mixed Agriculture', 
+                               'Agriculture'))
+cover_types <- data.frame(neighid=results$neighid, lctype=results$lctype)
+events <- merge(events, cover_types, by.x="nid", by.y="neighid")
+
+# First analyze first birth intervals:
+first_births <- events[events$event == "First birth", ]
+mean_fb_int_allruns <- aggregate(first_births$fb_int, by=list(year=first_births$year, 
+                                                        runname=first_births$runname, 
+                                                        lctype=first_births$lctype), 
+                                 mean)
+names(mean_fb_int_allruns)[grep('^x$', names(mean_fb_int_allruns))] <- 
+    'mean_fb_int'
+
+fb_int_means <- aggregate(mean_fb_int_allruns$mean_fb_int, 
+                          by=list(year=mean_fb_int_allruns$year, 
+                                  lctype=mean_fb_int_allruns$lctype), mean)
+names(fb_int_means)[grep('^x$', names(fb_int_means))] <- 'mean'
+fb_int_sds <- aggregate(mean_fb_int_allruns$mean_fb_int, 
+                        by=list(year=mean_fb_int_allruns$year, 
+                                lctype=mean_fb_int_allruns$lctype), sd)
+names(fb_int_sds)[grep('^x$', names(fb_int_sds))] <- 'sd'
+fb_int_sds$lower_lim <- fb_int_means$mean - (2 * fb_int_sds$sd)
+fb_int_sds$upper_lim <- fb_int_means$mean + (2 * fb_int_sds$sd)
+
+# Drop mixed classes:
+p <- ggplot()
+p + geom_line(aes(year, mean, colour=lctype), data=fb_int_means) +
+    geom_ribbon(aes(x=year, ymin=lower_lim, ymax=upper_lim, fill=lctype),
+        alpha=.2, data=fb_int_sds) +
+    scale_fill_discrete(legend=F) +
+    labs(x="Years", y='Time to First Birth (months)', colour="Cover Class")
+ggsave(paste(DATA_PATH, "first_birth_intervals.png", sep="/"), width=PLOT_WIDTH, 
+       height=PLOT_HEIGHT, dpi=300)
+
+# Drop mixed classes:
+fb_int_sds <- fb_int_sds[!grepl('Mixed', fb_int_sds$lctype), ]
+fb_int_means <- fb_int_means[!grepl('Mixed', fb_int_means$lctype), ]
+p <- ggplot()
+p + geom_line(aes(year, mean, colour=lctype), data=fb_int_means) +
+    geom_ribbon(aes(x=year, ymin=lower_lim, ymax=upper_lim, fill=lctype),
+        alpha=.2, data=fb_int_sds) +
+    scale_fill_discrete(legend=F) +
+    labs(x="Years", y='Time to First Birth (months)', colour="Cover Class")
+ggsave(paste(DATA_PATH, "first_birth_intervals_2_class.png", sep="/"), width=PLOT_WIDTH, 
+       height=PLOT_HEIGHT, dpi=300)
+
+# Now analyze marriage times
+marriages <- events[events$event == "Marriage", ]
+mean_marr_age_allruns <- aggregate(marriages$age, by=list(year=marriages$year, 
+                                                        runname=marriages$runname, 
+                                                        lctype=marriages$lctype), 
+                                 mean)
+names(mean_marr_age_allruns)[grep('^x$', names(mean_marr_age_allruns))] <- 
+    'mean_marr_age'
+
+marr_age_means <- aggregate(mean_marr_age_allruns$mean_marr_age, 
+                          by=list(year=mean_marr_age_allruns$year, 
+                                  lctype=mean_marr_age_allruns$lctype), mean)
+names(marr_age_means)[grep('^x$', names(marr_age_means))] <- 'mean'
+marr_age_sds <- aggregate(mean_marr_age_allruns$mean_marr_age, 
+                        by=list(year=mean_marr_age_allruns$year, 
+                                lctype=mean_marr_age_allruns$lctype), sd)
+names(marr_age_sds)[grep('^x$', names(marr_age_sds))] <- 'sd'
+marr_age_sds$lower_lim <- marr_age_means$mean - (2 * marr_age_sds$sd)
+marr_age_sds$upper_lim <- marr_age_means$mean + (2 * marr_age_sds$sd)
+
+# Drop mixed classes:
+p <- ggplot()
+p + geom_line(aes(year, mean, colour=lctype), data=marr_age_means) +
+    geom_ribbon(aes(x=year, ymin=lower_lim, ymax=upper_lim, fill=lctype),
+        alpha=.2, data=marr_age_sds) +
+    scale_fill_discrete(legend=F) +
+    labs(x="Years", y='Marriage Age (years)', colour="Cover Class")
+ggsave(paste(DATA_PATH, "marriage_age.png", sep="/"), width=PLOT_WIDTH, 
+       height=PLOT_HEIGHT, dpi=300)
+
+# Drop mixed classes:
+marr_age_sds <- marr_age_sds[!grepl('Mixed', marr_age_sds$lctype), ]
+marr_age_means <- marr_age_means[!grepl('Mixed', marr_age_means$lctype), ]
+p <- ggplot()
+p + geom_line(aes(year, mean, colour=lctype), data=marr_age_means) +
+    geom_ribbon(aes(x=year, ymin=lower_lim, ymax=upper_lim, fill=lctype),
+        alpha=.2, data=marr_age_sds) +
+    scale_fill_discrete(legend=F) +
+    labs(x="Years", y='Marriage Age (years)', colour="Cover Class")
+ggsave(paste(DATA_PATH, "marriage_age_2_class.png", sep="/"), width=PLOT_WIDTH, 
+       height=PLOT_HEIGHT, dpi=300)
+
+
