deb39fc177536f3e4c732f16df2198b80989b0f5
Alex Zvoleff <azvoleff@mail.sdsu.edu>
2013-01-22 20:44:00 -0800

    Clarify code.

--- chitwanabm/misc/fabfile.py
+++ chitwanabm/misc/fabfile.py
@@ -9,8 +9,7 @@ rcfile_path = "C:/Users/azvoleff/ChitwanABM/rcfiles"
 
 env.roledefs = {
     'manager': ['ubuntu@ec2-54-242-233-203.compute-1.amazonaws.com'],
-    'worker':  ['ubuntu@ec2-23-22-163-82.compute-1.amazonaws.com',
-                'ubuntu@ec2-54-242-72-224.compute-1.amazonaws.com']
+    'worker':  ['ubuntu@ec2-54-242-72-224.compute-1.amazonaws.com']
 }
 
 env.key_filename = "C:/Users/azvoleff/Keys/azvoleff_ec2_keypair.pem"
@@ -32,26 +31,29 @@ def update_workers():
 def start_workers():
     run('nohup /home/ubuntu/Code/chitwanabm/chitwanabm/misc/EC2_batch_run.sh &')
 
+@task
+def shutdown_workers():
+    run('shutdown -h now')
+
 class MyTask(Task):
     name = "deploy_rcfiles"
     def __init__(self, *args, **kwargs):
         super(MyTask, self).__init__(*args, **kwargs)
         num_workers = len(env.roledefs['worker'])
-        rcfiles = os.listdir(rcfile_path)
-        rcfiles = [os.path.join(rcfile_path, rcfile) for rcfile in rcfiles]
-        min_num = len(rcfiles) / num_workers
-        num_per_worker = [min_num] * num_workers
-        for n in range(0, len(num_per_worker)):
-            num_per_worker[n] += 1
-            if sum(num_per_worker) == len(rcfiles): break
+        rcfiles = [os.path.join(rcfile_path, rcfile) for rcfile in os.listdir(rcfile_path)]
+        jobs_per_worker = [len(rcfiles) / num_workers] * num_workers
+        for n in range(0, len(jobs_per_worker)):
+            jobs_per_worker[n] += 1
+            if sum(jobs_per_worker) == len(rcfiles): break
         self.worker_files = []
         index = 0
         for n in range(0, num_workers):
-            self.worker_files.append(rcfiles[index:(index + num_per_worker[n])])
+            self.worker_files.append(rcfiles[index:(index + jobs_per_worker[n])])
             index += num_per_worker[n]
 
     def run(self):
         for worker_file in self.worker_files[0]:
             put(worker_file, "/home/ubuntu/ChitwanABM/rcfiles")
+            put("C:/Users/azvoleff/pyabmrc_EC2", "/home/ubuntu/pyabmrc")
         del self.worker_files[0]
 instance = MyTask() 
