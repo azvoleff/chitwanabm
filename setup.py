cac98b04db94d9072f42fecb1132ef32e41f9e6f
Alex Zvoleff <azvoleff@outlook.com>
2013-06-10 13:06:04 -0700

    Add numexpr dependency.

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     install_requires = ['numpy >= 1.7.0',
                         'matplotlib >= 0.98.4',
                         'pyabm[gdal] >= 0.3',
+                        'numpexpr >- 2.0.0',
                         'tables >= 2.4.0'],
     author = "Alex Zvoleff",
     author_email = "azvoleff@mail.sdsu.edu",
